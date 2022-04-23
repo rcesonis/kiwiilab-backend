ee8365b8857b763c92936a13a394fcce4a8e9285
“rcesonis” <rimvydas.cesonis@gmail.com>
2022-04-23 19:40:50 +0200

    Configed express json parser

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
-const app = express();
 const { PORT } = require("./config/constants");
 const userRoute = require("./routers/user");
+
+// Create an express app
+const app = express();
+
+/* Middlewares: */
+
+// express.json():be able to read request bodies of JSON requests a.k.a. body-parser
+const bodyParserMiddleWare = express.json();
+app.use(bodyParserMiddleWare);
+
+/* Routes */
 
 app.use("/", userRoute);
 
